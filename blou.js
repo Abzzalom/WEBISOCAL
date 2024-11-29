8246157e15455c6876412eac0cdee7dd07560689
Erick José Sanchez Pinedo <erijito.2@gmail.com>
2024-11-28 23:27:19 -0500

    Responsive Blog-inicio-articulo

--- blou.js
+++ blou.js
@@ -1,15 +1,15 @@
-document.getElementById('inicio').addEventListener('click', function() {
+document.getElementById('inicio').addEventListener('click', function () {
     alert('Inicio clicked');
 });
 
-document.getElementById('noticias').addEventListener('click', function() {
+document.getElementById('noticias').addEventListener('click', function () {
     alert('Noticias clicked');
 });
 
-document.getElementById('webinar').addEventListener('click', function() {
+document.getElementById('webinar').addEventListener('click', function () {
     alert('Webinar clicked');
 });
 
-document.getElementById('cursos').addEventListener('click', function() {
+document.getElementById('cursos').addEventListener('click', function () {
     alert('Cursos clicked');
 });
